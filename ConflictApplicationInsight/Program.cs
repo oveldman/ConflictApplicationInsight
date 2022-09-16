9da09ad8c7bf73b0137ade923a3dc19677825c98
Oscar Veldman <oveldman@gmail.com>
2022-09-16 10:59:46 +0200

    Remove application insight

--- ConflictApplicationInsight/Program.cs
+++ ConflictApplicationInsight/Program.cs
@@ -1,4 +1,3 @@
-using BlazorApplicationInsights;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ConflictApplicationInsight;
@@ -8,7 +7,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
-
-builder.Services.AddBlazorApplicationInsights();
 
 await builder.Build().RunAsync();
